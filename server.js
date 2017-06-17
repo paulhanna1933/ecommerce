be38e0a5ba980eaff4d1368163a28ae14c9b88d2
Paul Hanna <paulhanna1933@gmail.com>
2017-06-18 01:11:00 +0300

    update
    after passport configuration

--- server.js
+++ server.js
@@ -7,12 +7,15 @@ var engine        = require('ejs-mate');
 var session       = require('express-session');
 var cookieParser  = require('cookie-parser');
 var flash         = require('express-flash');
-
+var MongoStore    = require('connect-mongo/es5')(session);
+var passport      = require('passport');
 
 var secret = require('./config/secret');
 var User = require('./models/user');
 
 var app = express(); // to get express commands with app.
+
+
 
 mongoose.connect(secret.database, function(err) {
   if(err) {
@@ -31,9 +34,12 @@ app.use(cookieParser());
 app.use(session({
   resave: true,
   saveUninitialized: true,
-  secret: secret.secretKey
+  secret: secret.secretKey,
+  store: new MongoStore({ url: secret.database, autoReconnect: true})
 }));
 app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.engine('ejs', engine);
 app.set('view engine', 'ejs');
