f0f652c796fb23ee2806f1f3c2bc53edfc44678c
Paul Hanna <paulhanna1933@gmail.com>
2017-06-17 23:37:59 +0300

    Version 1
    Prior to configuring passport

---
+++ config/secret.js
@@ -0,0 +1,6 @@
+module.exports = {
+
+  database: 'mongodb://root:abc123@ds127982.mlab.com:27982/ecommerce',
+  port: 3000,
+  secretKey: "Paul333"
+}
--- server.js
+++ server.js
@@ -1,21 +1,50 @@
-var express = require ('express'); // telling server to use express
-var morgan = require ('morgan');
+var express       = require('express'); // telling server to use express
+var morgan        = require('morgan');
+var mongoose      = require('mongoose');
+var bodyParser    = require('body-parser');
+var ejs           = require('ejs');
+var engine        = require('ejs-mate');
+var session       = require('express-session');
+var cookieParser  = require('cookie-parser');
+var flash         = require('express-flash');
+
+
+var secret = require('./config/secret');
+var User = require('./models/user');
 
 var app = express(); // to get express commands with app.
 
+mongoose.connect(secret.database, function(err) {
+  if(err) {
+    console.log(err);
+  } else {
+    console.log('connected to the database');
+  }
+});
+
 //Middleware
+app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(cookieParser());
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: secret.secretKey
+}));
+app.use(flash());
 
-app.get('/', function(req, res) {
-  var name = "Batman";
-  res.json("My name is " + name);
-});
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
 
-app.get('/catname', function(req, res){
-  res.json('batman');
-});
+var mainRoutes = require('./routes/main');
+var userRoutes = require('./routes/user');
 
-app.listen(3000, function(err) {  // viewable URL at localhost:3000
+app.use(mainRoutes);
+app.use(userRoutes);
+
+app.listen(secret.port, function(err) {  // viewable URL at localhost:3000
   if (err) throw err;
-  console.log("Server is Running");
+  console.log("Server is Running on port " + secret.port);
 });
