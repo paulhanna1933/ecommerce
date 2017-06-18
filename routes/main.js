1492db1c3fd6ba3f5c3f553f40fc032aa54f95eb
Paul Hanna <paulhanna1933@gmail.com>
2017-06-18 21:15:15 +0300

    update

--- routes/main.js
+++ routes/main.js
@@ -1,5 +1,6 @@
 var router = require('express').Router();
 var User = require('../models/user');
+var Product = require('../models/product');
 
 
 router.get('/', function(req, res) {
@@ -12,12 +13,25 @@ router.get('/about', function(req, res){
 
 
 
-router.get('/users', function(req, res) {
-  User.find({}, function(err, users) {
-    res.json(users);
-  })
-})
+router.get('/products/:id', function(req, res, next) {
+  Product
+  .find({ category: req.params.id })
+  .populate('category')
+  .exec(function(err, products) {
+    if (err) return next(err);
+    res.render('main/category', {
+      products: products
+    });
+  });
+});
 
-
+router.get('/product/:id', function(req, res, next) {
+  Product.findById({_id: req.params.id }, function(err, product) {
+    if (err) return next(err);
+    res.render('main/product', {
+      product: product
+    });
+  });
+});
 
 module.exports = router;
